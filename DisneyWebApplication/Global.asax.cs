cde2eb3d666a3c2178c40acb1b6220ff5f302d08
Manuel-Rodrigo Cabello Malagon <Manuel-Rodrigo Cabello Malagon>
2016-04-03 15:15:02 +0200

    disabling minifications

--- DisneyWebApplication/Global.asax.cs
+++ DisneyWebApplication/Global.asax.cs
@@ -12,10 +12,11 @@ namespace DisneyWebApplication
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+            AreaRegistration.RegisterAllAreas();            
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
